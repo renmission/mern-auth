894707bb8776626a2ede077057339f1989ddb115
renmission <missionrenjr@gmail.com>
2024-07-17 06:11:39 +0800

    completed signup functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -13,7 +13,7 @@ export const signUpController = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save();
-        res.status(200).json({
+        res.status(201).json({
             message: 'User created successfully'
         });
     } catch (error) {
