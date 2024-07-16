354967a5b402fdae29f4477266fa1d325eca9152
renmission <missionrenjr@gmail.com>
2024-07-16 09:25:56 +0800

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      TEST
-    </div>
+   <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='sign-in' element={<SignIn />} />
+        <Route path='sign-up' element={<SignUp />} />
+        <Route path='profile' element={<Profile />} />
+      </Routes>
+   </Router>
   )
 }
 
