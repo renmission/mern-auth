13e1aa0a557493d5cf4d12e814ca409b7343e77e
renmission <missionrenjr@gmail.com>
2024-07-24 14:54:01 +0800

    Updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { Link } from 'react-router-dom'; 
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -8,12 +7,12 @@ const Footer = () => {
     <footer className="fixed bottom-0 w-full py-4 bg-white flex justify-center items-center">
        <div className='flex flex-col items-center space-y-2'>
         <div className='flex justify-center'>
-                <Link to={{ pathname: "https://github.com/renmission/mern-auth" }} target="_blank" className="mr-2">
+                <a href="https://github.com/renmission/mern-auth" target="_blank" rel="noopener noreferrer" className="mr-2">
                     <FaGithub size={24} className="text-slate-400" />
-                </Link>
-                <Link to={{ pathname: "https://www.linkedin.com/in/renmission/" }} target="_blank" className="mr-2">
+                </a>
+                <a href="https://www.linkedin.com/in/renmission/" target="_blank" rel="noopener noreferrer" className="mr-2">
                     <FaLinkedin size={24} className="text-slate-400" />
-                </Link>
+                </a>
             </div>
             <p className="text-center text-slate-400">
             Copyright &copy; {currentYear} Renato Mission
